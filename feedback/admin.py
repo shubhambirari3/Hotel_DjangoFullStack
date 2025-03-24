0ee52e026d2f987c304101ba24373460694290fe
shubhambirari3 <shubhambirari3@gmail.com>
2025-03-24 15:09:32 +0530

    24:added feedback

--- feedback/admin.py
+++ feedback/admin.py
@@ -3,7 +3,7 @@ from .models import Feedback
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'room', 'rating', 'comment', 'country','created_at')
+    list_display = ('name', 'email', 'room', 'rating', 'country','created_at')
     search_fields = ('name', 'email', 'room__name')
     list_filter = ('rating', 'created_at')
     ordering = ('-created_at',)
